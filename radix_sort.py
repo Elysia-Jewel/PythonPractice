527fc211a18e281fc42a98b9c20a0bc3b8060721
elee0081 <elee0081@student.monash.edu>
2024-04-03 13:57:15 +0800

    optimised radix sort

--- radix_sort.py
+++ radix_sort.py
@@ -8,15 +8,29 @@ def radix_sort(lst, base):
         exp += 1
     return lst
 
+## Padding with special characters. Cons: additional complexity to evaluate paddings, additional complexity to remove paddings from strings after sorting
 def radix_string(lst):
+
     return lst
 
-def radix_string_unequal(lst):
+## Without padding. Worst-case time complexity: O(N) where N is the sum of the characters of the strings in the list.
+def radix_string_optimal(lst):
+    #sort strings according to length
+    for i in range(1, len(lst)):
+        j = i - 1
+        while j >= 0:
+            if len(lst[j+1]) < len(lst[j]):
+                lst[j+1], lst[j] = lst[j], lst[j+1]
+            j -= 1
+    #radix sort from bottom up, skipping the strings after when first empty character is detected in a column
+
     return lst
 
 if __name__ == '__main__':
     test1 = [8,3,7,41,19,10]
     test2 = [34, 85, 19, 74, 25]
     test3 = [200, 151, 291, 981, 369, 421, 671]
-    print('My algorithm: ', radix_sort(test3[:], 10))
-    print('Compare: ', sorted(test3))
+    test4 = ['dog', 'taro', 'cat', 'gudetama', 'elk'] #['cat', 'dog', 'elk', 'gudetama', 'taro']
+    test5 = [170, 45, 75, 90, 802, 24, 2, 66]
+    print('My algorithm: ', radix_string_optimal(test4[:]))
+    print('Compare: ', sorted(test4))
